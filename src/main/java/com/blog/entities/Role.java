9a573cc3ca176f66152f890a0466d961cd9dc344
sumangalam <sumanglamb@gmail.com>
2025-04-04 13:15:06 +0530

    Blogging-application-api

---
+++ src/main/java/com/blog/entities/Role.java
@@ -0,0 +1,18 @@
+package com.blog.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Role {
+	
+	@Id
+	private Integer id;
+	private String name;
+	
+
+}
